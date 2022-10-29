d2a7ee7f2f9d2fbd41aa2c232a05a21bfc48d7ae
Lyuba Pushkina <lyuba.pushkina@gmail.com>
2022-10-29 18:08:35 +0100

    redux-toolkit

--- src/redux/Books/booksSelect.js
+++ src/redux/Books/booksSelect.js
@@ -1,1 +1,1 @@
-export const selectBooks = state => state.books;
+export const selectBooks = state => state.books.books;
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,39 @@
-import { createStore, combineReducers } from 'redux';
+/* import { createStore, combineReducers } from 'redux';
 import { devToolsEnhancer } from '@redux-devtools/extension';
-import { booksReducer } from './Books/booksReducer';
-
+import { booksReducer } from './Books/booksReducer'; */
+import { configureStore } from '@reduxjs/toolkit';
+// import { booksReducer } from './Books/booksReducer';
+import booksReducer from './Books/booksSlice';
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+/* 
 const enhancer = devToolsEnhancer();
 const rootReducer = combineReducers({ books: booksReducer });
-export const store = createStore(rootReducer, enhancer);
+export const store = createStore(rootReducer, enhancer); */
+
+const persistConfig = {
+  key: 'books',
+  storage,
+};
+const persistedReducer = persistReducer(persistConfig, booksReducer);
+export const store = configureStore({
+  reducer: {
+    books: persistedReducer,
+  },
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+});
+export const persistor = persistStore(store);
